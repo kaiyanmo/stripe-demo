29af48473d11edd576c554cefb309632a9053a2d
Kai Yan Mo <mokaiyan@gmail.com>
2026-09-22 00:41:56 +1000

    Prepare Stripe demo for cloud deployment

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const Stripe = require('stripe');
 const app = express();
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-const PORT = 3000;
+// Cloud hosting will provide PORT.
+// Locally, default to 3000.
+const PORT = process.env.PORT || 3000;
 
 // Serve files from /public
 app.use(express.static('public'));
@@ -14,6 +16,13 @@ app.use(express.static('public'));
 // Create a Stripe Checkout Session
 app.post('/create-checkout-session', async (req, res) => {
   try {
+
+    // Automatically determine whether we're running locally
+    // or behind HTTPS on the deployed site.
+    const protocol = req.get('x-forwarded-proto') || req.protocol;
+    const host = req.get('host');
+    const baseUrl = `${protocol}://${host}`;
+
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
 
@@ -24,8 +33,11 @@ app.post('/create-checkout-session', async (req, res) => {
         },
       ],
 
-      success_url: `http://localhost:${PORT}/success.html`,
-      cancel_url: `http://localhost:${PORT}/`,
+      success_url:
+        `${baseUrl}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+
+      cancel_url:
+        `${baseUrl}/`,
     });
 
     console.log('Checkout Session created:', session.id);
@@ -39,5 +51,5 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Demo merchant running at http://localhost:${PORT}`);
+  console.log(`Stripe demo merchant running on port ${PORT}`);
 });
